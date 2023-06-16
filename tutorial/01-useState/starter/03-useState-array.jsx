c86920ee55eda48f0ff3c002e1ba9d3f42db436d
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-16 15:29:47 -0700

    Using state to alter/update array

--- tutorial/01-useState/starter/03-useState-array.jsx
+++ tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,37 @@
+import { data } from "../../../src/data.js";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+	const [people, setPeople] = React.useState(data);
+
+	const removeItem = (id) => {
+		console.log(id);
+		const newPeople = people.filter((person) => person.id !== id);
+		setPeople(newPeople);
+	};
+	const clearAll = () => {
+		setPeople([]);
+		// setPeople(people.filter((person) => person.id !== id)); alternative
+	};
+
+	return (
+		<div>
+			{people.map((person) => {
+				const { id, name } = person;
+
+				return (
+					<div key="id">
+						<h2>{name}</h2>
+						<button onClick={() => removeItem(id)}>Remove</button>
+					</div>
+				);
+			})}
+			{/* <button style={{ marginTop: "2rem" }} className="btn" onClick={()=> setPeople([])}> alternative method */}
+			<button style={{ marginTop: "2rem" }} className="btn" onClick={clearAll}>
+				Clear All
+			</button>
+		</div>
+	);
 };
 
 export default UseStateArray;
