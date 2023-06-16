22e1a6b4b82f118f189f4f612014ab5062a6dc60
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-16 15:59:57 -0700

    Updating Objects with State

--- tutorial/01-useState/starter/04-useState-object.jsx
+++ tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,49 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	// const [name, setName] = useState("peter");
+	// const [age, setAge] = useState(24);
+	// const [hobby, setHobby] = useState("reading books");
+
+	const [person, setPerson] = useState({
+		name: "peter",
+		age: 24,
+		hobby: "Reading books",
+	});
+
+	const displayJohn = () => {
+		// setName("John");
+		// setAge(28);
+		// setHobby("Screaming at Computer");
+
+		setPerson({
+			name: "John",
+			age: 28,
+			hobby: "Screaming at computers",
+		});
+	};
+
+	const displaySusie = () => {
+		setPerson({ ...person, name: "Susie" });
+	};
+
+	return (
+		<>
+			<h3>{person.name}</h3>
+			<h3>{person.age}</h3>
+			<h4>Hobby: {person.hobby}</h4>
+			<button className="btn" onClick={displayJohn}>
+				Show John
+			</button>
+			<button
+				className="btn"
+				onClick={displaySusie}
+				style={{ marginLeft: "2rem" }}
+			>
+				Show Susan
+			</button>
+		</>
+	);
 };
 
 export default UseStateObject;
