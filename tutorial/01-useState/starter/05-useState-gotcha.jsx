4fbcf663e015b9c6fe91f52f28ed79d1a3b11c5a
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-16 19:14:28 -0700

    Updating state synchronically using current/new state

--- tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,29 @@
+// Value logged is asynchronous to value shown (+1)
+
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+	const [value, setValue] = useState(0);
+
+	const handleClick = () => {
+		// setValue(value + 1);
+		setValue((currentState) => {
+			console.log("Current: " + currentState);
+			const newState = currentState + 1;
+			console.log("New: " + newState);
+
+			return newState;
+		});
+	};
+
+	return (
+		<div>
+			<h1>{value}</h1>
+			<button type="button" className="btn" onClick={handleClick}>
+				++
+			</button>
+		</div>
+	);
 };
 
 export default UseStateGotcha;
