04c91af5eaa9bd99beb2d6881c924e98a2e47ef0
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-16 21:25:38 -0700

    Exploration: useEffect()

--- tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0);
-  const sayHello = () => {
-    console.log('hello there');
-  };
+	const [value, setValue] = useState(0);
+	const sayHello = () => {
+		// Infinite Loop: setValue(value+1);
+		console.log("hello there");
+	};
 
-  sayHello();
+	sayHello();
 
-  return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
-  );
+	useEffect(() => {
+		// const someFunc = async () => {
+		// 	await fetch;
+		// };
+		console.log("useEffect");
+	}, []);
+
+	return (
+		<div>
+			<h1>value : {value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</div>
+	);
 };
 export default UseEffectBasics;
