5b280c75c3d4a00aa745c6b2f3c149d846878b4a
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-22 10:49:59 -0700

    Default/Custom states using Truthy/Falsies - Username

--- tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
-  // falsy
-  const [text, setText] = useState('');
-  // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+	// falsy
+	const [text, setText] = useState("");
+	// truthy
+	const [name, setName] = useState("susan");
+	const [user, setUser] = useState({ name: "john" });
+	const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+	return (
+		<div>
+			<h2>{text || "Default Value"}</h2>
+			{text && (
+				<div>
+					<h2>Things</h2>
+					<h2>{name}</h2>
+				</div>
+			)}
+			{/* {!text && (
+				<div>
+					<h2>!Things</h2>
+					<h2>{name}</h2>
+				</div>
+			)} */}
+			{user && <SomeComponent name={user.name} />}
+		</div>
+	);
+};
+
+const SomeComponent = ({ name }) => {
+	return (
+		<div>
+			<h2>Things</h2>
+			<h2>{name}</h2>
+		</div>
+	);
 };
 
 export default ShortCircuitExamples;
