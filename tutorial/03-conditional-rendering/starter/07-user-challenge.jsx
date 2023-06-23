44c83ae4b4471264005949c5a589789ba2d7bc00
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-23 13:57:24 -0700

    Update interface depending on if a user is logged in or not.

--- tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+	const [user, setUser] = useState(null);
+
+	const login = () => {
+		setUser({ name: "vegan food truck" });
+	};
+
+	const logout = () => {
+		setUser(null);
+	};
+
+	return (
+		<div>
+			{user ? (
+				<div>
+					<h4>Hello there, {user.name}</h4>
+					<button className="btn" onClick={logout}>
+						Log Out
+					</button>
+				</div>
+			) : (
+				<div>
+					<h4>Hello there, stranger</h4>
+					<button className="btn" onClick={login}>
+						Log In
+					</button>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default UserChallenge;
