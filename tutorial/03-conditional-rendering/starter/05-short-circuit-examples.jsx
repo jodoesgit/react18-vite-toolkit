4783dff891499a31f01435b3777740eda7a24a38
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-23 13:40:56 -0700

    Discovery: Ternary conditional values - login - display user/login

--- tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -5,8 +5,9 @@ const ShortCircuitExamples = () => {
 	const [text, setText] = useState("");
 	// truthy
 	const [name, setName] = useState("susan");
-	const [user, setUser] = useState({ name: "john" });
-	const [isEditing, setIsEditing] = useState(false);
+	// const [user, setUser] = useState({ name: "john" });
+	const [user, setUser] = useState(null);
+	const [isEditing, setIsEditing] = useState(true);
 
 	return (
 		<div>
@@ -24,6 +25,17 @@ const ShortCircuitExamples = () => {
 				</div>
 			)} */}
 			{user && <SomeComponent name={user.name} />}
+			<h2 style={{ margin: "1rem 0" }}>Ternary Operator</h2>
+			<button className="btn">{isEditing ? "edit" : "add"}</button>
+			{user ? (
+				<div>
+					<h4>Hello there, {user.name}</h4>
+				</div>
+			) : (
+				<div>
+					<h2>Please login</h2>
+				</div>
+			)}
 		</div>
 	);
 };
