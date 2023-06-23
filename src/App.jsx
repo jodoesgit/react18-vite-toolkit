ee929581f28009f01b97d9850b361c59142fcc9d
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-23 14:38:28 -0700

    Bulk importing components through index/pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Starter from "../tutorial/02-useEffect/starter/05-cleanup-function";
+import { Home, About } from "../tutorial/04-project-structure/starter/pages";
 
 function App() {
 	return (
 		<div className="container">
-			<Starter />
+			<Home />
+			<About />
 		</div>
 	);
 }
