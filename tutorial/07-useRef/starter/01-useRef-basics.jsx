45ca9c46b20836a1d63e5c92bc50c10fa9742679
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-24 16:00:55 -0700

    useRef to trigger mounting/unmounting of objects

--- tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,14 +3,23 @@ import { useEffect, useRef, useState } from "react";
 const UseRefBasics = () => {
 	const [value, setValue] = useState(0);
 	const refContainer = useRef(null);
+	const isMounted = useRef(false);
 
 	useEffect(() => {
-		console.log(refContainer);
+		refContainer.current.focus();
 	});
+
+	useEffect(() => {
+		if (!isMounted.current) {
+			isMounted.current = true;
+			return;
+		}
+		console.log("re-rendered");
+	}, [value]);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const name = refContainer.current.value;
-		console.log(name);
 	};
 
 	return (
