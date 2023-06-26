55b3006cbbb0347021ada213a062218037f81daa
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-26 14:46:42 -0700

    Using Memo to stop triggering re-render on unaltered components

--- tutorial/11-performance/starter/03-hooks/index.jsx
+++ tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState } from "react";
+import { data } from "../../../../src/data";
+import List from "./List";
 const LowerState = () => {
-  const [people, setPeople] = useState(data);
-  const [count, setCount] = useState(0);
+	const [people, setPeople] = useState(data);
+	const [count, setCount] = useState(0);
 
-  return (
-    <section>
-      <button
-        className='btn'
-        onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
-      >
-        count {count}
-      </button>
-      <List people={people} />
-    </section>
-  );
+	return (
+		<section className="container">
+			<button
+				className="btn"
+				onClick={() => setCount(count + 1)}
+				style={{ marginBottom: "1rem" }}
+			>
+				count {count}
+			</button>
+			<List people={people} />
+		</section>
+	);
 };
 export default LowerState;
