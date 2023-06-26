2f1bd2f0737794efae0281a74351dfc52482e819
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-25 21:10:00 -0700

    Added reset functionality to list, prepared to use a Reducer

--- tutorial/10-useReducer/starter/01-useReducer.jsx
+++ tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../src/data";
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+	const [people, setPeople] = React.useState(data);
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
-  return (
-    <div>
-      {people.map((person) => {
-        const { id, name } = person;
-        return (
-          <div key={id} className='item'>
-            <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
-          </div>
-        );
-      })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
-    </div>
-  );
+	const removeItem = (id) => {
+		let newPeople = people.filter((person) => person.id !== id);
+		setPeople(newPeople);
+	};
+
+	const clearList = () => {
+		setPeople([]);
+	};
+
+	const resetList = () => {
+		setPeople(data);
+	};
+
+	return (
+		<div class="container">
+			{people.map((person) => {
+				const { id, name } = person;
+				return (
+					<div key={id} className="item">
+						<h4>{name}</h4>
+						<button onClick={() => removeItem(id)}>remove</button>
+					</div>
+				);
+			})}
+			{people.length < 1 ? (
+				<button
+					className="btn"
+					style={{ marginTop: "2rem" }}
+					onClick={resetList}
+				>
+					reset items
+				</button>
+			) : (
+				<button
+					className="btn"
+					style={{ marginTop: "2rem" }}
+					onClick={clearList}
+				>
+					clear items
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default ReducerBasics;
